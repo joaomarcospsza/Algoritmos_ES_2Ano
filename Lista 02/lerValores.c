e12e2dbe7fd7852eb1f7e3c0ce57134253f46f35
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2023-02-17 16:08:04 -0300

    Exercício 05

---
+++ Lista 02/lerValores.c
@@ -0,0 +1,21 @@
+/*Faça um programa em C que leia 5 valores e determine o menor deles.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int i, count = 5, numero, menor = 0;
+
+    for (i = 0; i < count; i++)
+    {
+        printf("Digite o valor do %dº numero: ", i+1);
+        scanf("%d", &numero);
+
+        if(numero > menor){
+            menor = numero;
+        }  
+    }
+    printf("O maior numero digitado foi: %d", menor);
+    return 0;
+}
