f4eeaf0a67538ca49bbcf8615e45690cff8d1143
João Marcos <112454993+joaomarcospsza@users.noreply.github.com>
2023-02-17 18:32:12 -0300

    Exercício 02 - Ler 2 valores
    Escreva um programa em C para ler 2 valores (considere que não serão informados valores
    iguais) e escrever o maior deles.

---
+++ Lista 02/Maior_Valor.c
@@ -0,0 +1,35 @@
+/* Escreva um programa em C para ler 2 valores (considere que não serão informados valores
+iguais) e escrever o maior deles. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int num1, num2, maior;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &num1);
+    printf("Digite o segundo valor: ");
+    scanf("%d", &num2);
+
+    while (num1 == num2)
+    {
+        printf("Valores identicos.\n");
+        printf("Por Favor, informe o primeiro valor novamente: ");
+        scanf("%d", &num1);
+    }
+
+    if (num1 > num2)
+    {
+        maior = num1;
+    }
+    else
+    {
+        maior = num2;
+    }
+
+    printf("O maior valor e: %d\n", maior);
+    return 0;
+}
